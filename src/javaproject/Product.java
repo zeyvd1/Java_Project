345606c54dbc5890897b1f013a10321bc3f30518
zuksh <zivdvhmed5@gmail.com>
2026-05-18 16:45:58 +0300

    Update Product.java

--- src/javaproject/Product.java
+++ src/javaproject/Product.java
@@ -1,4 +1,3 @@
-
 package javaproject;
 
 public abstract class Product {
@@ -10,12 +9,14 @@ public abstract class Product {
     protected int quantity;
     protected int rating;
     protected int reviews;
-   
-    public Product(){}
-    
+
+    // ← مضاف: لتخزين اختيار اللون/الـ size اللي اختاره المستخدم
+    protected String displayTag = "";
+
+    public Product() {}
+
     public Product(int id, String name, double price, Discount discount,
-            String description, int quantity ,int rating ,int reviews) {
-        
+            String description, int quantity, int rating, int reviews) {
         this.id = id;
         this.name = name;
         this.price = price;
@@ -24,16 +25,17 @@ public abstract class Product {
         this.quantity = quantity;
         this.rating = rating;
         this.reviews = reviews;
-       
     }
+
     public double applyDiscount() {
-
-        if(discount == null)
+        if (discount == null)
             return price;
-
         return discount.applyDiscount(price);
     }
 
+    // ← مضاف
+    public void setDisplayTag(String tag) { this.displayTag = tag; }
+    public String getDisplayTag()         { return displayTag; }
+
     public abstract double getFinalPrice();
 }
-    
